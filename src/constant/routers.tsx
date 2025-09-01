4361ae3009e41728c97f506aa27c7d761b6071ec
ShubhamPandey77 <shubhampandeys084@gmail.com>
2025-09-01 10:49:23 +0530

    adding routes in authprotetcted

--- src/constant/routers.tsx
+++ src/constant/routers.tsx
@@ -7,34 +7,27 @@ import Cart from "../pages/Cart/Cart.page";
 import BuyNow from "../pages/Buy/Buy.page";
 import type { JSX } from "react";
 
-// We check for token in a function instead of top-level constant
+
 function isUserAuthenticated(): boolean {
   return Boolean(localStorage.getItem("authToken"));
 }
 
 const router = createBrowserRouter([
   { path: "/", element: <LoginUser /> },
-  {
-    path: "/home",
-    element: (
-      <AuthProtected isAuthenticated={isUserAuthenticated()}>
-        <Home />
-      </AuthProtected>
-    ),
-  },
-  {
-    path: "/products/:id",
-    element: <ProductDetail />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-    {
-    path: "/buy",
-    element: <BuyNow />,
-  },
-]);
+{
+  element: (
+    <AuthProtected isAuthenticated={isUserAuthenticated()}>
+      <></> 
+    </AuthProtected>
+  ),
+  children: [
+    { path: "/home", element: <Home /> },
+    { path: "/products/:id", element: <ProductDetail /> },
+    { path: "/cart", element: <Cart /> },
+    { path: "/buy", element: <BuyNow /> },
+  ],
+}
+])
 
 function Routers(): JSX.Element {
   return <RouterProvider router={router} />;
